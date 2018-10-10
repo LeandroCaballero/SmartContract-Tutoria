c6d284dd98ff7482621d904ce94c664210625ed3
LeandroCaballero <leandrorojo248@hotmail.com>
2018-10-10 19:19:30 -0300

    se agregan las funciones 'cancelar' y 'estaCancelado'

--- tutoria.sol
+++ tutoria.sol
@@ -41,17 +41,47 @@ contract Tutoria {
     
     function confirmar(address llave) public returns (bool) {
         datosTutoria t = tutorias[llave];
-        
+    
+    
 	require (msg.sender == t.profesor); 
 
 	t.estaConf = true;
         
         
+
 	return t.estaConf;
     }
     
     function estaConfirmado() public returns (bool){
         require(tutorias[msg.sender].estaConf == true);
+
         return tutorias[msg.sender].estaConf;
+    }
+
+    function cancelar(address llave) public returns (bool) {
+  
+      
+        
+	require(tutorias[llave].alumno == msg.sender); 
+ 
+       
+        
+	require(tutorias[llave].estaCancel == false);
+   
+     
+        
+	require(tutorias[llave].estaConf == false);
+     
+   
+        
+	return tutorias[llave].estaCancel = true;
+    
+    }
+    
+    function estaCancelado(address llave) public view returns (bool){
+    
+    
+	return tutorias[llave].estaCancel;
+    
     }
 }
